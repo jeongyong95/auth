b038708b6e2e0508bbd0d6aa0f9374be4f9190f0
jeongyong <jeongyong95@gmail.com>
2022-05-16 19:48:57 +0900

    [AUTH-0000] jjwt 의존성 추가

--- auth-core/build.gradle.kts
+++ auth-core/build.gradle.kts
@@ -9,9 +9,9 @@ tasks.getByName<Jar>("jar") {
 }
 
 dependencies {
-    implementation("io.jsonwebtoken:jjwt-api")
-    implementation("io.jsonwebtoken:jjwt-impl")
-    implementation("io.jsonwebtoken:jjwt-jackson")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
